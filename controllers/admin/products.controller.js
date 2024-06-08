0bd9c3fe5c399e92fb70dc337e31850288a4b8a0
Minh Quang GM Dev <quangminhgdev204@gmail.com>
2024-06-08 15:40:39 +0700

    Tính năng lọc theo trạng thái bên backend

--- controllers/admin/products.controller.js
+++ controllers/admin/products.controller.js
@@ -1,11 +1,43 @@
 // [GET] /admin/products
 const Product = require("../../models/product.model");
-module.exports.index = async (req ,res) => { 
-    const products = await Product.find({
-        deleted : false
-    }); // đi tìm những bản ghi đi tìm những bản ghi có deleted = false
+module.exports.index = async (req ,res) => {
+    const find = {
+        deleted: false
+      };
+    
+      const filterStatus = [
+        {
+          name: "Tất cả",
+          status: "",
+          class: ""
+        },
+        {
+          name: "Hoạt động",
+          status: "active",
+          class: ""
+        },
+        {
+          name: "Dừng hoạt động",
+          status: "inactive",
+          class: ""
+        }
+      ];
+    
+      if(req.query.status) {
+        const index = filterStatus.findIndex(item => item.status == req.query.status);
+        filterStatus[index].class = "active";
+      } else {
+        const index = filterStatus.findIndex(item => item.status == "");
+        filterStatus[index].class = "active";
+      }
+    
+      if(req.query.status) {
+        find.status = req.query.status;
+      } 
+      const products = await Product.find(find);
     res.render("admin/pages/products/index.pug" , {
         pageTitle :"Danh Sach Sản Phẩm",
-        products : products
+        products : products,
+        filterStatus: filterStatus
     });
 }
