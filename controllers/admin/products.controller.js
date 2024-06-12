abc42527c46c5460f06f9691e74a4feb03e3de91
Minh Quang GM Dev <quangminhgdev204@gmail.com>
2024-06-12 21:56:36 +0700

    Fix lại phần lọc theo trạng thái

--- controllers/admin/products.controller.js
+++ controllers/admin/products.controller.js
@@ -4,36 +4,25 @@ module.exports.index = async (req ,res) => {
     const find = {
         deleted: false
       };
-    
+    // Lọc theo trạng thái
       const filterStatus = [
         {
           name: "Tất cả",
           status: "",
-          class: ""
         },
         {
           name: "Hoạt động",
           status: "active",
-          class: ""
         },
         {
           name: "Dừng hoạt động",
           status: "inactive",
-          class: ""
         }
-      ];
-    
-      if(req.query.status) {
-        const index = filterStatus.findIndex(item => item.status == req.query.status);
-        filterStatus[index].class = "active";
-      } else {
-        const index = filterStatus.findIndex(item => item.status == "");
-        filterStatus[index].class = "active";
-      }
-    
+      ];   
       if(req.query.status) {
         find.status = req.query.status;
-      } 
+      }
+      // end lọc theo trạng thái
       const products = await Product.find(find);
     res.render("admin/pages/products/index.pug" , {
         pageTitle :"Danh Sach Sản Phẩm",
---
+++ public/admin/js/script.js
@@ -0,0 +1,23 @@
+// Button Status
+const listButtonStatus = document.querySelectorAll("[button-status]");
+if(listButtonStatus.length > 0) {
+  let url = new URL(window.location.href);
+
+  listButtonStatus.forEach((button) => {
+    button.addEventListener("click", () => {
+      const status = button.getAttribute("button-status");
+      if(status) {
+        url.searchParams.set("status", status);
+      } else {
+        url.searchParams.delete("status");
+      }
+      window.location.href = url.href;
+    });
+  });
+
+  const statusCur = url.searchParams.get("status") || "" ;
+    const buttonCurrent = document.querySelector(`[button-status = "${statusCur}"]`);
+    buttonCurrent.classList.add("active");
+}
+
+// End Button Status
