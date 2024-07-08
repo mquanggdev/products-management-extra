f5e9c2e27d92fd20b307066bdb46c1cb100ccdaa
Minh Quang GM Dev <quangminhgdev204@gmail.com>
2024-07-08 21:24:00 +0700

    Bài 20.3 : 1 : Xây dựng phần quản lydanh mục sản phẩm (Phần Khung)

---
+++ controllers/admin/products-category.controller.js
@@ -0,0 +1,4 @@
+module.exports.index = async (req ,res) => {
+    res.render("admin/pages/products-category/index.pug" , {
+    });
+}
--- routes/admin/index.route.js
+++ routes/admin/index.route.js
@@ -1,8 +1,10 @@
 const dashboardRoutes = require("./dashboard.route.js");
 const systemConfig = require("../../config/system.js");
 const productsRoutes = require("./products.route.js");
+const productsCategoryRoutes = require("./products-category.route.js");
 module.exports = (app) => {
     const PATH_ADMIN = "/" + systemConfig.PREFIX_ADMIN; // mục đích tạo ra thằng này để mai sau khách hàng có muốn đổi tên đường dẫn thì cũng có thể linh động được.Lấy nó thông qua một file trong config
     app.use(PATH_ADMIN + "/dashboard" , dashboardRoutes);
     app.use(PATH_ADMIN + "/products" , productsRoutes);
+    app.use(PATH_ADMIN + "/products-category" , productsCategoryRoutes);
 }
