2099f50896296113a3f1ee3b855067ee2a1a7e91
Minh Quang GM Dev <quangminhgdev204@gmail.com>
2024-07-08 21:58:55 +0700

    Bài 20.3 : 2 : Xây dựng tính năng thêm mới danh mục sản phẩm (Giao diện và chức năng)

--- controllers/admin/products-category.controller.js
+++ controllers/admin/products-category.controller.js
@@ -1,4 +1,31 @@
+const ProductCategory = require("../../models/products-category.model");
+const systemConfig = require("../../config/system");
+
+// get/admin/products-category
 module.exports.index = async (req ,res) => {
     res.render("admin/pages/products-category/index.pug" , {
     });
+}
+// [GET]/admin/products-category/create
+module.exports.create = async (req ,res) => {
+    res.render("admin/pages/products-category/create.pug" , {
+    });
+}
+
+// [POST]/admin/products-category/createPost
+module.exports.createPost = async (req ,res) => {
+    try {
+      if(req.body.position) {
+        req.body.position = parseInt(req.body.position);
+      } else {
+        const countProducts = await ProductCategory.countDocuments({});
+        req.body.position = countProducts + 1;
+      }
+      const newProductCategory = new ProductCategory(req.body);
+      await newProductCategory.save();
+      req.flash("success" ,"Thêm danh mục sản phẩm thành công");
+      } catch (error) {
+        console.log(error);
+      }
+      res.redirect(`/${systemConfig.PREFIX_ADMIN}/products-category`);
 }
--- routes/admin/products-category.route.js
+++ routes/admin/products-category.route.js
@@ -1,7 +1,17 @@
 const express = require("express");
 const router = express.Router();
 const controller = require("../../controllers/admin/products-category.controller")
+const multer  = require('multer')
+const upload = multer();
+const uploadCloudMiddleware = require("../../middleware/admin/uploadCloud.middleware");
 
 router.get("/" , controller.index);
+router.get("/create",controller.create);
+router.post(
+    "/create",
+    upload.single("thumbnail"),
+    uploadCloudMiddleware.uploadSingle,
+    controller.createPost
+);
 
 module.exports = router;
