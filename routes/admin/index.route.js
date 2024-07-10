3c7974d8e84952bb311804b79ba50242b3958ee3
Minh Quang GM Dev <quangminhgdev204@gmail.com>
2024-07-10 15:55:09 +0700

    Bài 21.3 : 2 : Thêm phần danh sách nhóm quyền

--- routes/admin/index.route.js
+++ routes/admin/index.route.js
@@ -2,9 +2,11 @@ const dashboardRoutes = require("./dashboard.route.js");
 const systemConfig = require("../../config/system.js");
 const productsRoutes = require("./products.route.js");
 const productsCategoryRoutes = require("./products-category.route.js");
+const rolesRoutes = require("./roles.route");
 module.exports = (app) => {
     const PATH_ADMIN = "/" + systemConfig.PREFIX_ADMIN; // mục đích tạo ra thằng này để mai sau khách hàng có muốn đổi tên đường dẫn thì cũng có thể linh động được.Lấy nó thông qua một file trong config
     app.use(PATH_ADMIN + "/dashboard" , dashboardRoutes);
     app.use(PATH_ADMIN + "/products" , productsRoutes);
     app.use(PATH_ADMIN + "/products-category" , productsCategoryRoutes);
+    app.use(PATH_ADMIN + "/roles" , rolesRoutes);
 }
