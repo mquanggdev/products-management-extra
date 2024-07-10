19e0b8bb996c248574a8e3d0a12dfc3b04372618
Minh Quang GM Dev <quangminhgdev204@gmail.com>
2024-07-10 15:40:13 +0700

    Bài 21.2 : 1 : thêm collection roles cho dự án

---
+++ models/roles.model.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+const slug = require('mongoose-slug-updater');
+mongoose.plugin(slug);
+const rolesSchema = new mongoose.Schema({
+    title: String,
+    description: String,
+    permission:{
+        type:Array,
+        default:[]
+    },
+    deleted: {
+        type:Boolean,
+        default:false
+        }
+    },
+{
+    timestamps: true // Tự động thêm trường createdAt và updatedAt (https://mongoosejs.com/docs/timestamps.html)
+  });
+
+const Role = mongoose.model("Role" , rolesSchema , "roles");
+
+module.exports = Role ;
