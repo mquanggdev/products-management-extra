80a8e026caf3eb7d5dac9aac669dec529fb9eb8d
Minh Quang GM Dev <quangminhgdev204@gmail.com>
2024-07-10 16:03:43 +0700

    Bài 21.3 : 3 : Phần thêm mới các danh mục nhóm quyền

--- controllers/admin/roles.controller.js
+++ controllers/admin/roles.controller.js
@@ -1,4 +1,5 @@
 const Role = require("../../models/roles.model");
+const systemConfig = require("../../config/system");
 
 // [GET] /admin/roles
 module.exports.index = async (req, res) => {
@@ -11,3 +12,24 @@ module.exports.index = async (req, res) => {
     records: records
   });
 }
+
+// [GET] /admin/roles/create
+module.exports.create = async (req, res) => {
+    res.render("admin/pages/roles/create.pug", {
+        pageTitle: "Thêm Mới Nhóm quyền"
+      });
+}
+
+// [Post] /admin/roles/create
+module.exports.createPost = async (req, res) => {
+    if(req.body){
+        const newRoles = new Role(req.body);
+        await newRoles.save();
+
+        req.flash("success","Thêm mới sản phầm thành công")
+        res.redirect(`/${systemConfig.PREFIX_ADMIN}/roles`);
+    }
+    else{
+        req.redirect(`/${systemConfig.PREFIX_ADMIN}/roles`);
+    }
+  }
--- routes/admin/roles.route.js
+++ routes/admin/roles.route.js
@@ -4,5 +4,7 @@ const router = express.Router();
 const controller = require("../../controllers/admin/roles.controller");
 
 router.get("/", controller.index);
+router.get("/create", controller.create);
+router.post("/create", controller.createPost);
 
 module.exports = router;
