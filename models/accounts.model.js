4fd48433b179cccaf353799451c7485551c07617
Minh Quang GM Dev <quangminhgdev204@gmail.com>
2024-07-14 14:59:22 +0700

    Bài 22.1 : Tạo collection cho phần accounts

---
+++ models/accounts.model.js
@@ -0,0 +1,20 @@
+const accountSchema = new mongoose.Schema({
+    fullName: String,
+    email: String,
+    phone: String,
+    password: String,
+    token: String,
+    avatar: String,
+    role_id: String,
+    status: String,
+    deleted: {
+      type: Boolean,
+      default: false
+    },
+  }, {
+    timestamps: true
+  });
+  
+  const Account = mongoose.model("Account", accountSchema, "accounts");
+  
+  module.exports = Account;
