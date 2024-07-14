bb768713361b35da08cc3fbb3bae1d2482419a69
Minh Quang GM Dev <quangminhgdev204@gmail.com>
2024-07-14 15:39:54 +0700

    Bài 21.6 : Tính năng đăng nhập

--- routes/admin/index.route.js
+++ routes/admin/index.route.js
@@ -4,6 +4,7 @@ const productsRoutes = require("./products.route.js");
 const productsCategoryRoutes = require("./products-category.route.js");
 const rolesRoutes = require("./roles.route");
 const accountsRoutes = require("./accounts.route");
+const authRoutes = require("./auth.route");
 module.exports = (app) => {
     const PATH_ADMIN = "/" + systemConfig.PREFIX_ADMIN; // mục đích tạo ra thằng này để mai sau khách hàng có muốn đổi tên đường dẫn thì cũng có thể linh động được.Lấy nó thông qua một file trong config
     app.use(PATH_ADMIN + "/dashboard" , dashboardRoutes);
@@ -11,4 +12,5 @@ module.exports = (app) => {
     app.use(PATH_ADMIN + "/products-category" , productsCategoryRoutes);
     app.use(PATH_ADMIN + "/roles" , rolesRoutes);
     app.use(PATH_ADMIN + "/accounts" , accountsRoutes);
+    app.use(PATH_ADMIN + "/auth" , authRoutes);
 }
