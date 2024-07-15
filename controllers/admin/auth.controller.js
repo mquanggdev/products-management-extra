0300f3a99ebdfdacb8d8cb33e67834c5205bf154
Minh Quang GM Dev <quangminhgdev204@gmail.com>
2024-07-15 13:31:52 +0700

    Bài 21.7 : Tính năng đăng xuất

--- controllers/admin/auth.controller.js
+++ controllers/admin/auth.controller.js
@@ -38,5 +38,11 @@ module.exports.loginPost = async (req, res) => {
   }
 
   res.cookie("token", account.token);
-  res.redirect(`/${systemConfig.prefixAdmin}/dashboard`);
+  res.redirect(`/${systemConfig.PREFIX_ADMIN}/dashboard`);
+}
+
+// [GET] /admin/auth/logout
+module.exports.logout = async (req, res) => {
+  res.clearCookie("token");
+  res.redirect(`/${systemConfig.PREFIX_ADMIN}/auth/login`);
 }
