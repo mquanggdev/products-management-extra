55ab1acffe55ff613a9e5cce6fe48726b57268e1
Minh Quang GM Dev <quangminhgdev204@gmail.com>
2024-07-24 16:34:39 +0700

    Bài 25.3 : Làm Trang giỏ hàng

--- routes/client/carts.route.js
+++ routes/client/carts.route.js
@@ -3,6 +3,7 @@ const router = express.Router();
 
 const controller = require("../../controllers/client/carts.controller");
 
+router.get("/", controller.index);
 router.post("/add/:productId", controller.addPost);
 
 module.exports = router;
