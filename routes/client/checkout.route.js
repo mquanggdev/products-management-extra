2ec5721d8cabb73e993dfc8d6f70448adfa40353
Minh Quang GM Dev <quangminhgdev204@gmail.com>
2024-07-26 13:56:39 +0700

    Bài 26.4 : Hiển thị đơn hàng được đặt hàng thành công

--- routes/client/checkout.route.js
+++ routes/client/checkout.route.js
@@ -5,5 +5,5 @@ const controller = require("../../controllers/client/checkout.controller");
 
 router.get("/", controller.index);
 router.post("/order", controller.orderPost);
-
+router.get("/success/:orderId", controller.success);
 module.exports = router;
