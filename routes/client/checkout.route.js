8736fd6882093f5910b5f72e181429998d24eebf
Minh Quang GM Dev <quangminhgdev204@gmail.com>
2024-07-26 13:02:33 +0700

    Bài 26.3 : Lưu dữ liệu vào database thông tin khách hàng và sản phẩm với order

--- routes/client/checkout.route.js
+++ routes/client/checkout.route.js
@@ -4,5 +4,6 @@ const router = express.Router();
 const controller = require("../../controllers/client/checkout.controller");
 
 router.get("/", controller.index);
+router.post("/order", controller.orderPost);
 
 module.exports = router;
