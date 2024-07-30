d9cb104288aead31db0dc142d8e5a7ef94141da2
Minh Quang GM Dev <quangminhgdev204@gmail.com>
2024-07-30 13:17:06 +0700

    Bài 26.2 : Hoàn thiện phần đăng ký tài khoản user (Bao gồm check email và lưu và data base và thêm tokenUser)

--- controllers/client/user.controller.js
+++ controllers/client/user.controller.js
@@ -1,6 +1,7 @@
 const md5 = require("md5");
 
 const generateHelper = require("../../helper/generate.helper");
+const User = require("../../models/user.model");
 
 // [GET] /user/register
 module.exports.register = async (req, res) => {
@@ -11,14 +12,27 @@ module.exports.register = async (req, res) => {
 
 // [POST] /user/register
 module.exports.registerPost = async (req, res) => {
+  const existUser = await User.findOne({
+    email:req.body.email,
+    deleted:false,
+  })
+  if(existUser){
+    req.flash("error" , "Email đã tồn tại")
+    res.redirect("back");
+    return;
+  }
   const userData = {
     fullName: req.body.fullName,
     email: req.body.email,
     password: md5(req.body.password),
     tokenUser: generateHelper.generateRandomString(30)
   };
+  
+  const user = new User(userData);
+  await user.save() ;
 
-  console.log(userData);
-
-  res.send("OK");
+  res.cookie("tokenUser" , user.tokenUser);
+  
+  req.flash("success", "Đăng ký tài khoản thành công!");
+  res.redirect("/");
 };
