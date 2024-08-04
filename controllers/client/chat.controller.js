5a31d86685a70918965c7dd6414b8f982441f256
Minh Quang GM Dev <quangminhgdev204@gmail.com>
2024-08-04 15:11:37 +0700

    Bài 28.4 : Buổi 1 Socket.io :Làm tính năng chat cơ bản

--- controllers/client/chat.controller.js
+++ controllers/client/chat.controller.js
@@ -1,9 +1,34 @@
+const Chat = require("../../models/chat.model");
+const User = require("../../models/user.model");
 //GET /chat 
 module.exports.index = async (req,res) => {
-    _io.on("connection" , (socket) => {
-        console.log("Có 1 người vừa kết nối " , socket.id);
+  const userId = res.locals.user.id ;
+    _io.once("connection" , (socket) => {
+      socket.on("CLIENT_SEND_MESSAGE" ,async (data) => {
+        console.log(data); 
+          const chatData = {
+            userId : userId,
+            content:data.content
+          }       
+          // lưu data vào database
+          const chat = new Chat(chatData) ;
+          await chat.save();
+      })
+
+      // Trả tin nhắn realtime về cho mn
     });
+
+    const chats = await Chat.find({});
+    for (const chat of chats) {
+      const infoUser = await User.findOne({
+        _id : chat.userId
+      })
+
+      chat.fullname = infoUser.fullName;
+    } 
+    
     res.render("client/pages/chat/index.pug",{
       pageTitle : "Chat",
+      chats: chats
     });
   }
